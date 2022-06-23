8db08d494543e1f61f5d5e5fb5fe92a6d71e5053
Question Mark <64643214+SkvernoSlovie@users.noreply.github.com>
2022-06-23 09:52:13 +0300

    fix default port value

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function start() {
-  const PORT = process.env.PORT || 8080;
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule, { cors: true });
 
 
